5758e8db66dbf116d4c1daaf8947dd093aa1a88a
M2KV <90041846+M2KV@users.noreply.github.com>
2021-09-22 14:03:47 +0700

    index

--- main.js
+++ main.js
@@ -7,6 +7,9 @@ document.getElementsByClassName('notepad-closebtn')[0].addEventListener('click',
 document.getElementsByClassName('st_practices')[0].addEventListener('click', function(){
     document.getElementsByClassName('practices')[0].classList.remove('hiddens');    
 });
+document.getElementsByClassName('st_resources')[0].addEventListener('click', function(){
+    document.getElementsByClassName('resources')[0].classList.remove('hiddens');    
+});
 document.getElementsByClassName('notepad-closebtn')[1].addEventListener('click', function(){
     document.getElementsByClassName('practices')[0].classList.add('hiddens');    
 });
